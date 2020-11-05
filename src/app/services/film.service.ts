4ab3803ee2ba02966aa4f18340113f345ad20f11
Théo <theo.mouton@axopen.com>
2020-11-05 16:26:58 +0100

    ajout token

--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FilmService {
+  private url = 'https://api.themoviedb.org/3/';
+  private token = 'api_key=52ff9827d9c7caeb290a41c3ea052989';
 
   constructor() { }
+
 }
